d09ddf8efac80594d83e8f3b2d4e5c4ea5ee2c7b
Chris Milliren <milli079@umn.edu>
2022-12-03 14:08:27 -0600

    updates to flowcharts

--- Pumpcontrol_flowchart.py
+++ Pumpcontrol_flowchart.py
@@ -6,7 +6,7 @@ with schemdraw.Drawing(backend='svg') as d:
     d.config(fontsize=10, unit=1.5)
     d += flow.Terminal().label('DAQ Computer').drop('E')
     d += flow.Arrow().label('USB')
-    d += flow.Box().label('FTDI COM4')
+    d += flow.Box().label('FTDI USB-\nCOM485-PLUS4')
     d += flow.Arrow().label('RS485\nModbus')
     d += flow.Box().label('ADAM 4024 AO')
     d += flow.Arrow().label('0-10VDC')
