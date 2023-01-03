4724e1c4f6ff4b210fb6f8789476e2265aeeb33d
Chris Milliren <milli079@umn.edu>
2023-01-03 16:16:48 -0600

    updates

--- Sedflux_control_funcs.py
+++ Sedflux_control_funcs.py
@@ -1,18 +1,32 @@
 
 class sed_flux_control():
-    def __init__(self,do_obj):
-        self.do = do_obj
+    def __init__(self,dump_weight):
+        self.dump_weight = dump_weight #lbs
+
+    def update(self,do_obj,sct_obj):
+        sct_obj.read_weights()
+
+        if sct_obj.net_weight > self.dump_weight:
+            do_obj.dump_sed()
+
 
 if __name__ == '__main__':
 
-    import ADAM_4024_AnalogOut as adam
     import Arduino_DigitalOutput as arduino
+    import SCT1100_ASCII_funcs as sct1100
+    import time
 
-    ao = adam.adam4024('COM15',1)
     do = arduino.digout('COM14')
-    fill_pump = vfd(0,ao,do)
-    fill_pump.set_freq(50)
-    fill_pump.start_vfd()
+    sct = sct1100.SCT1100('COM5')
+    
+    sedflux = sed_flux_control(1)
 
+    try:
+        while True:
+            sedflux.update(do_obj=do,sct_obj=sct)
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print('Stopping')
 
 
